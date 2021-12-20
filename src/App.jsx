b1a43c7702c7e7fd291e58db595c234608861c2d
Yaw Botwe <ybotwe@gmail.com>
2021-12-20 13:27:30 +0000

    initialize sdk & connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
+
 const App = () => {
+
+  const { connectWallet, address, error, provider} = useWeb3();
+  console.log('👋🏾 Address:', address);
+
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to CharityDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero btn-black">
+          Connect with Metamask
+        </button>
+        <br></br>
+        <button onClick={() => connectWallet("walletconnect")} className="btn-hero btn-blue">
+          Connect with WalletConnect
+        </button>
+        <br></br>
+        <button onClick={() => connectWallet("walletlink")} className="btn-hero btn-white">
+          Connect with Coinbase Wallet
+        </button>
+        <br></br>
+      </div>
+    );
+  }
+
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+//Import 3rdweb
+
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks';
+
+const supportedChains = [3,4];
+
+const connectors = {
+  injected: {},
+  walletconnect: {},
+  walletlink: {
+    appName: "charitydao",
+    url: "https://thirdweb.com",
+    darkMode: false,
+  },
+};
+
+
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider 
+      connectors={connectors} 
+      supportedChains={supportedChains}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
