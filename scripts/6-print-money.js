49bad6e68e9bf84d8e398a9b3ae4ac01ada0e5de
Yaw Botwe <ybotwe@gmail.com>
2021-12-21 20:52:21 +0000

    print money

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -1,3 +1,21 @@
 import { ethers } from "ethers";
 import sdk from "./1-initialize-sdk.js";
 
+const tokenModule = sdk.getTokenModule('0x7A7de1E769D99a1a4BA18Cb2a1418457A7A24584');
+
+(async () => {
+    try{
+        const amount = 100_000_000;
+        const amountWith18Decimals = ethers.utils.parseUnits(amount.toString(), 18);
+        await tokenModule.mint(amountWith18Decimals);
+        const totalSupply = await tokenModule.totalSupply();
+
+        console.log(
+            "✅ There now is",
+            ethers.utils.formatUnits(totalSupply, 18),
+            "$GOOD in circulation",
+        );
+    } catch(error){
+        console.error("Failed to print money", error);
+    }
+})();
